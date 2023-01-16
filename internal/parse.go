c50b6b3586498ef769b1d11807bc3c13ad596f2f
Джованни Муньос <egmn472@yandex.com>
2023-01-16 11:31:57 +0300

    проверка получения значения

--- internal/parse.go
+++ internal/parse.go
@@ -14,7 +14,10 @@ type parsewav struct {
 	сhunkRIFF     string
 	сhunkSizeRIFF int
 	сhunkWave     string
-	сhunkFmt      string
+
+	//  fmt
+	сhunkFmt     string
+	сhunkSizeFmt int
 
 	// для создания объекта wav
 	buildwav model.BuilderWav
@@ -34,25 +37,36 @@ func NewParser(bw model.BuilderWav) model.Parser {
 
 func (pw *parsewav) Parse(dw *[]byte) (model.Wave, error) {
 
+	// cлово RIFF
 	pw.сhunkRIFF = string((*dw)[model.IdxStartWordRiff:model.IdxEndWordRiff])
 	if pw.сhunkRIFF != string(model.ConstRIFF) {
 		return nil, errors.New("слово RIFF не найдено. Значение: " + pw.сhunkRIFF)
 	}
 
+	// size RIFF
 	strSzRiff := string((*dw)[model.IdxStartChunkSzRiff:model.IdxEndChunkSzRiff])
 	pw.сhunkSizeRIFF, pw.err = strconv.Atoi(strSzRiff)
 	if pw.err != nil {
-		return nil, errors.New("ошибка при получании sizeRiff. Значение: " + pw.сhunkWave)
+		return nil, errors.New("ошибка при получении sizeRiff. Значение: " + strconv.Itoa(pw.сhunkSizeRIFF))
 	}
 
+	// слово WAVE
 	pw.сhunkWave = string((*dw)[model.IdxStartWordWave:model.IdxEndWordWave])
 	if pw.сhunkWave != string(model.ConstWAVE) {
 		return nil, errors.New("слово WAVE не найдено. Значение: " + pw.сhunkWave)
 	}
 
+	// cлово fmt
 	pw.сhunkFmt = string((*dw)[model.IdxStartWordFmt:model.IdxEndWordFmt])
 	if pw.сhunkRIFF != string(model.ConstRIFF) {
 		return nil, errors.New("слово FMT не найдено. Значение:" + pw.сhunkFmt)
+	}
+
+	// size fmt
+	strSzFmt := string((*dw)[model.IdxStartChunkSzFmt:model.IdxEndChunkSzFmt])
+	pw.сhunkSizeFmt, pw.err = strconv.Atoi(strSzFmt)
+	if pw.err != nil {
+		return nil, errors.New("ошибка при получении sizeRiff. Значение: " + strconv.Itoa(pw.сhunkSizeFmt))
 	}
 
 	//pw.сhunkRIFF[0]
